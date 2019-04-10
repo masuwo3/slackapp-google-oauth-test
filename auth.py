2a725a21a6b09496d2338e1fcc30dbffdd0d2d8b
cm-hirata.kei <hirata.kei@classmethod.jp>
2019-04-10 12:47:21 +0900

    response_urlを使ってemailを返すように変更

--- auth.py
+++ auth.py
@@ -4,6 +4,7 @@ import hashlib
 import json
 from urllib.parse import urlencode
 from urllib.parse import urljoin
+from urllib.parse import parse_qs
 
 GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
 SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
@@ -14,33 +15,26 @@ SCOPE = 'email'
 def invoke(event, context):
     headers = event['headers']
     body_raw = event['body']
+    req_ctx = event['requestContext']
 
+    # Slack Appから送られたリクエストかどうかを判別
     if not verify_request(headers, body_raw, SLACK_SIGNING_SECRET):
         resp = json.dumps({
                     "response_type": "ephemeral",
-                    "text": "Request is unverified."})
+                    "text": "Request is Invalid."})
         return {'statusCode': 200, 'body': resp}
 
-    domain = event['requestContext']['domainName']
-    path = event['requestContext']['path']
-    redirect_uri = urljoin('https://' + domain + path, 'callback')
+    # API GatewayのContextからOAuth認証用のリダイレクトURLを作成
+    redirect_uri = __redirect_uri(req_ctx)
+
+    # slackのresponse_urlをリダイレクト後も参照できるようにstateに渡す
+    state = {'response_url': parse_qs(body_raw)['response_url']}
+    oauth_link = __oauth_link(redirect_uri, state)
+
+    body = {'text': '<{}|Googleアカウントでログイン>'.format(oauth_link)}
 
     return {"statusCode": 200,
-            "body": json.dumps({
-                        "text": __oauth_link(redirect_uri)})}
-
-
-def __oauth_link(redirect_uri, state=None):
-    url = 'https://accounts.google.com/o/oauth2/v2/auth?'
-    query = {'client_id': GOOGLE_CLIENT_ID,
-             'redirect_uri': redirect_uri,
-             'scope': SCOPE,
-             'response_type': 'code'}
-
-    if state:
-        query['state'] = state
-
-    return url + urlencode(query)
+            "body": json.dumps(body)}
 
 
 def verify_request(headers, body, signing_secret):
@@ -53,3 +47,21 @@ def verify_request(headers, body, signing_secret):
                                 hashlib.sha256).hexdigest()
 
     return hmac.compare_digest(req_hash, signature)
+
+
+def __oauth_link(redirect_uri, state=None):
+    url = 'https://accounts.google.com/o/oauth2/v2/auth?'
+    query = {'client_id': GOOGLE_CLIENT_ID,
+             'redirect_uri': redirect_uri,
+             'scope': SCOPE,
+             'response_type': 'code'}
+
+    if state:
+        query['state'] = json.dumps(state)
+
+    return url + urlencode(query)
+
+
+def __redirect_uri(req_ctx):
+    this_uri = 'https://' + req_ctx['domainName'] + req_ctx['path']
+    return urljoin(this_uri, 'callback')
--- callback.py
+++ callback.py
@@ -1,4 +1,6 @@
 import os
+import json
+
 import requests
 from requests import HTTPError
 
@@ -8,12 +10,17 @@ GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
 
 def invoke(event, context):
     q = event['queryStringParameters']
+    print('query: {}'.format(q))
+
     code = q['code']
 
-    domain = event['requestContext']['domainName']
-    path = event['requestContext']['path']
-    redirect_uri = 'https://' + domain + path
+    state = json.loads(q['state'])
+    print('state: {}'.format(state))
 
+    response_url = state['response_url'][0]
+    print('resp_url: {}'.format(response_url))
+
+    redirect_uri = __redirect_uri(event['requestContext'])
     data = {'code': code,
             'client_id': GOOGLE_CLIENT_ID,
             'client_secret': GOOGLE_CLIENT_SECRET,
@@ -21,18 +28,33 @@ def invoke(event, context):
             'grant_type': 'authorization_code'}
 
     try:
+        # codeをaccess tokenに変換する
         r = requests.post('https://www.googleapis.com/oauth2/v4/token',
                           data=data)
 
         r.raise_for_status()
         access_token = r.json()['access_token']
 
+        # access tokenを使ってGoogle APIからemailを取得する
         rr = requests.get('https://www.googleapis.com/oauth2/v1/userinfo',
                           headers={'Authorization': 'Bearer ' + access_token})
+        rr.raise_for_status()
         email = rr.json()['email']
+
+        # emailをslackのresponse_urlに送る
+        rrr = requests.post(response_url,
+                            headers={'Content-Type': 'application/json'},
+                            data=json.dumps({'text': f'あなたのメールアドレスは {email}'}))
+
+        rrr.raise_for_status()
     except HTTPError as e:
-        print(r.text)
+        print(f"status code: {e.response.status_code}")
+        print(f"text: {e.response.text}")
         raise e
 
     return {"statusCode": 200,
-            "body": email}
+            "body": 'ok'}
+
+
+def __redirect_uri(req_ctx):
+    return 'https://' + req_ctx['domainName'] + req_ctx['path']
