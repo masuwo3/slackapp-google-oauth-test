7c1a065d346d1199b4aa8f83dfc311c62f8b13ab
cm-hirata.kei <hirata.kei@classmethod.jp>
2019-05-19 17:59:19 +0900

    SlashCommandの作成メソッドをFactoryに切り出し

--- code/slack.py
+++ code/slack.py
@@ -3,26 +3,56 @@ import hmac
 import hashlib
 
 
+class SlashCommandFactory:
+    def __init__(self, sigining_secret=None):
+        self.sigining_secret = sigining_secret
+
+    def load_from_event(self, event):
+        payload = parse_qs(event['body'])
+
+        ts = event['headers']['X-Slack-Request-Timestamp']
+        sig = event['headers']['X-Slack-Signature']
+        body_raw = event['body']
+        resp_url = payload['response_url'][0]
+        name = payload['command'][0].lstrip('/')
+        # 送られたコマンドに引数がない場合は、空文字のlistを加える
+        params = payload['text'][0].split() if 'text' in payload else ['']
+
+        return SlashCommand(body=body_raw, timestamp=ts, 
+                            signature=sig, response_url=resp_url,
+                            name=name, params=params,
+                            sigining_secret=self.sigining_secret)
+
+    def load_from_state(self, state):
+        resp_url = state['response_url'][0]
+        name = state['command'][0]
+        params = state['text'][0]
+
+        return SlashCommand(response_url=resp_url, name=name, params=params)
+
+
 class SlashCommand:
-    def __init__(self, sigining_secret):
-        self.secret = sigining_secret
-
-    def load_event(self, event):
-        headers = event['headers']
-        payload_raw = event['body']
-        payload = parse_qs(payload_raw)
-
-        self.timestamp = headers['X-Slack-Request-Timestamp']
-        self.signature = headers['X-Slack-Signature']
-        self.payload_raw = payload_raw
-        self.response_url = payload['response_url'][0]
-        self.command_name = payload['command'][0].lstrip('/')
-        # 引数がない場合は、空文字のlistを加える
-        self.params = payload['text'][0].split() if 'text' in payload else ['']
+    def __init__(self, body=None, timestamp=None, signature=None,
+                 response_url=None, name=None, params=None,
+                 sigining_secret=None, **args):
+        self.body = body
+        self.timestamp = timestamp
+        self.signature = signature
+        self.response_url = response_url
+        self.name = name
+        self.params = params
+        self.sigining_secret = sigining_secret
+        self.extra = args
 
     def verify_request(self):
-        req = str.encode('v0:' + self.timestamp + ':' + self.payload_raw)
-        req_hash = 'v0=' + hmac.new(str.encode(self.secret),
+        if (self.sigining_secret is None
+                or self.signature is None
+                or self.body is None
+                or self.timestamp is None):
+            return False
+
+        req = str.encode('v0:' + self.timestamp + ':' + self.body)
+        req_hash = 'v0=' + hmac.new(str.encode(self.sigining_secret),
                                     req,
                                     hashlib.sha256).hexdigest()
 
@@ -30,5 +60,6 @@ class SlashCommand:
 
     def dump_state(self):
         return {'response_url': self.response_url,
-                'command': self.command_name,
+                'command': self.name,
                 'text': self.params}
+
--- test/test_slack.py
+++ test/test_slack.py
@@ -1,43 +1,65 @@
 from unittest import TestCase
 
 from code.slack import SlashCommand
+from code.slack import SlashCommandFactory
 
 
 class TestSlashCommand(TestCase):
     def setUp(self):
-        self.c = SlashCommand('SigingSecret')
+        self.c = SlashCommand(
+            body='command=/test&text=param1%20param2&response_url=https://hooks.slack.com/commands/1234/5678',  # noqa:E501
+            response_url='https://hooks.slack.com/commands/1234/5678',
+            timestamp='1234567890',
+            sigining_secret='abcd1234',
+            signature='v0=4700da1669cac1f44451880f3b5668e85bb0782af521580543feb77d3e47a5fc',  # noqa: E501
+            name='test',
+            params=['param1', 'param2'])
+
+    def test_load_from_event(self):
         event = {
             'headers': {
                 'X-Slack-Request-Timestamp': '12345678',
                 'X-Slack-Signature': 'slacksignature'},
             'body': 'command=/test&text=param1%20param2&response_url=https://hooks.slack.com/commands/1234/5678'}  # noqa: E501
-        self.c.load_event(event)
 
-    def test_load_event(self):
-        self.assertEquals(self.c.signature, 'slacksignature')
-        self.assertEquals(self.c.timestamp, '12345678')
-        self.assertEquals(self.c.payload_raw,
-                          'command=/test&text=param1%20param2&response_url=https://hooks.slack.com/commands/1234/5678')  # noqa: E501
-        self.assertEquals(self.c.response_url,
-                          'https://hooks.slack.com/commands/1234/5678')
-        self.assertEquals(self.c.command_name, 'test')
-        self.assertEquals(self.c.params, ['param1', 'param2'])
+        c = SlashCommandFactory('SigingSecret').load_from_event(event)
+
+        self.assertEquals(c.signature, 'slacksignature')
+        self.assertEquals(c.timestamp, '12345678')
+        self.assertEquals(c.body, 'command=/test&text=param1%20param2&response_url=https://hooks.slack.com/commands/1234/5678')  # noqa: E501
+        self.assertEquals(c.response_url, 'https://hooks.slack.com/commands/1234/5678')  # noqa: E501
+        self.assertEquals(c.name, 'test')
+        self.assertEquals(c.params, ['param1', 'param2'])
+
+    def test_load_from_event_with_noparams(self):
+        event = {
+            'headers': {
+                'X-Slack-Request-Timestamp': '12345678',
+                'X-Slack-Signature': 'slacksignature'},
+            'body': 'command=/test&response_url=https://hooks.slack.com/commands/1234/5678'}  # noqa: E501
+
+        c = SlashCommandFactory('SigingSecret').load_from_event(event)
+
+        self.assertEquals(c.params, [''])
+
+    def test_load_from_state(self):
+        state = {'response_url': ['https://hooks.slack.com/commands/abcd/efgh'],
+                 'command': ['test2'],
+                 'text': [['hoge', 'fuga']]}
+
+        c = SlashCommandFactory().load_from_state(state)
+
+        self.assertEquals(c.response_url, 'https://hooks.slack.com/commands/abcd/efgh')
+        self.assertEquals(c.name, 'test2')
+        self.assertEquals(c.params, ['hoge', 'fuga'])
 
     def test_verify(self):
-        c = SlashCommand('abcd1234')
-        c.payload_raw = 'command=/test&text=param1%20param2&response_url=https://hooks.slack.com/commands/1234/5678'  # noqa: E501
-        c.timestamp = '1234567890'
-        c.signature = 'v0=4700da1669cac1f44451880f3b5668e85bb0782af521580543feb77d3e47a5fc'  # noqa: E501
-
-        self.assertTrue(c.verify_request())
+        self.assertTrue(self.c.verify_request())
 
     def test_verify_fail(self):
-        c = SlashCommand('abcd123456')
-        c.payload_raw = 'command=/test&text=param1%20param2&response_url=https://hooks.slack.com/commands/1234/5678'  # noqa: E501
-        c.timestamp = '1234567890'
-        c.signature = 'v0=4700da1669cac1f44451880f3b5668e85bb0782af521580543feb77d3e47a5fc'  # noqa: E501
+        self.c.sigining_secret = 'abcd123456'
 
-        self.assertFalse(c.verify_request())
+        self.assertFalse(self.c.verify_request())
 
     def test_dump_state(self):
         state = self.c.dump_state()
@@ -45,17 +67,3 @@ class TestSlashCommand(TestCase):
         self.assertEquals(state, {'response_url': 'https://hooks.slack.com/commands/1234/5678',  # noqa: E501
                                   'command': 'test',
                                   'text': ['param1', 'param2']})
-
-    def test_dump_state_noparams(self):
-        event = {
-            'headers': {
-                'X-Slack-Request-Timestamp': '12345678',
-                'X-Slack-Signature': 'slacksignature'},
-            'body': 'command=/test&response_url=https://hooks.slack.com/commands/1234/5678'}  # noqa: E501
-
-        self.c.load_event(event)
-        state = self.c.dump_state()
-
-        self.assertEquals(state, {'response_url': 'https://hooks.slack.com/commands/1234/5678',  # noqa: E501
-                                  'command': 'test',
-                                  'text': ['']})
