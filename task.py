2f7f1a7d9f3f995391598c88f02591a8f4d83ae9
cm-hirata.kei <hirata.kei@classmethod.jp>
2019-05-23 19:59:16 +0900

    タスク失敗時にslackにメッセージを送る

--- task.py
+++ task.py
@@ -12,29 +12,38 @@ TASK_QUEUE_ENDPOINT = os.environ['TASK_QUEUE_ENDPOINT']
 def invoke(event, context):
     for record in event['Records']:
         task_params = json.loads(record['body'])
-
-        access_token = task_params['g_access_token']
-        email = get_email(access_token)
-
         response_url = task_params['response_url']
-        __post_response(response_url,
-                        {'text': f'あなたのアドレスは {email} です。'})
+        try:
+            access_token = task_params['g_access_token']
+            email = get_email(access_token)
 
-    return 'ok'
+            __post_response(response_url,
+                            {'text': f'あなたのアドレスは {email} です。'})
+
+            return 'ok'
+        except HTTPError as e:
+            print(f"status code: {e.response.status_code}")
+            print(f"text: {e.response.text}")
+            __post_response(response_url,
+                            {'text': f'タスクに失敗しました : {e.response.text}'})
+            raise e
 
 
 def get_email(access_token):
     credentials = Credentials(access_token)
     session = AuthorizedSession(credentials)
     response = session.get('https://www.googleapis.com/oauth2/v1/userinfo')
+    response.raise_for_status()
+
     return response.json()['email']
 
 
 def __post_response(response_url, data):
     try:
-        requests.post(response_url,
-                      headers={'Content-Type': 'application/json'},
-                      data=json.dumps(data))
+        r = requests.post(response_url,
+                          headers={'Content-Type': 'application/json'},
+                          data=json.dumps(data))
+        r.raise_for_status()
     except HTTPError as e:
         print(f"status code: {e.response.status_code}")
         print(f"text: {e.response.text}")
